3415048c24e5274021b21c9d47ef2c098b006924
Farhad32155 <farhadhossain3540@gmail.com>
2023-06-11 07:53:13 +0600

    functionality-not-working

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -12,3 +12,40 @@ document.addEventListener('keydown', function (event) {
         virtualKey.classList.add('matched');
     }
 })
+
+const testElement = document.getElementById('test-data');
+const inputElement = document.getElementById('keyboard');
+
+inputElement.addEventListener('input', () => {
+    const arrayText = testElement.querySelectorAll('span')
+    const arrayValue = inputElement.value.split('')
+
+    let correct = true
+    arrayText.forEach((characterSpan, index) => {
+        const character = arrayValue[index]
+        if (character == null) {
+            characterSpan.classList.remove('correct')
+            characterSpan.classList.remove('incorrect')
+            correct = false
+        } else if (character === characterSpan.innerText) {
+            characterSpan.classList.add('correct')
+            characterSpan.classList.remove('incorrect')
+        } else {
+            characterSpan.classList.remove('correct')
+            characterSpan.classList.add('incorrect')
+            correct = false
+        }
+    })
+
+})
+
+
+
+
+testElement.split('').forEach(character => {
+    const characterSpan = document.createElement('span')
+    characterSpan.innerText = character
+    testElement.appendChild(characterSpan)
+})
+inputElement.value = null
+startTimer()
